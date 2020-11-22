9922cea9388fba15e5fb2223d45c881b4914158a
mitsunari-soad <hengpingshitian292@gmail.com>
2020-11-22 18:45:24 +0900

    修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須' }
-    validates :first_yomigana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'フリガナは、全角（カタカナ）での入力が必須' }
-    validates :last_yomigana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'フリガナは、全角（カタカナ）での入力が必須' }
     validates :birth
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'フリガナは、全角（カタカナ）での入力が必須' } do
+    validates :first_yomigana
+    validates :last_yomigana
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: '半角英数混合(半角英語のみ)'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password { password }
+    password { Faker::Internet.password(min_length: 6)}
     password_confirmation { password }
     first_name { '漢字' }
     last_name { '漢字' }
     first_yomigana { 'アアア' }
     last_yomigana { 'アアア' }
-    birth { 11_111_111 }
+    birth { 2010_11_11 }
   end
 end
