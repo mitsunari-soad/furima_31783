1b8a4ffdc80b84df605e72244495364ff1166758
mitsunari-soad <hengpingshitian292@gmail.com>
2020-11-24 17:53:32 +0900

    修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @items = Item.order('created_at DESC')
   end
 
   def new
-    authenticate_user!
     @item = Item.new
   end
 
